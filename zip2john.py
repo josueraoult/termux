5c5764b773cbdbe4349c510788488d4498bf73af
josueraoult <103897395+josueraoult@users.noreply.github.com>
2024-05-31 00:38:13 +0200

    Update zip2john.py

--- zip2john.py
+++ zip2john.py
@@ -1,74 +1,53 @@
-import sqlite3
 import getpass
+from colorama import init, Fore, Style
 
-# Initialiser et configurer la base de données
-def init_db():
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL
-        )
-    ''')
-    conn.commit()
-    conn.close()
+# Initialisation de colorama
+init(autoreset=True)
+
+# Stockage des utilisateurs en mémoire (dictionnaire)
+users = {}
 
 # Inscription d'un nouvel utilisateur
 def register_user():
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
+    print(Fore.GREEN + "=== Inscription ===")
+    username = input(Fore.CYAN + "Entrez votre nom d'utilisateur: ")
+    password = getpass.getpass(Fore.CYAN + "Entrez votre mot de passe: ")
     
-    username = input("Entrez votre nom d'utilisateur: ")
-    password = getpass.getpass("Entrez votre mot de passe: ")
-    
-    try:
-        cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
-        conn.commit()
-        print("Inscription réussie!")
-    except sqlite3.IntegrityError:
-        print("Erreur: Ce nom d'utilisateur est déjà pris.")
-    
-    conn.close()
+    if username in users:
+        print(Fore.RED + "Erreur: Ce nom d'utilisateur est déjà pris.")
+    else:
+        users[username] = password
+        print(Fore.GREEN + "Inscription réussie!")
 
 # Connexion d'un utilisateur existant
 def login_user():
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
+    print(Fore.GREEN + "=== Connexion ===")
+    username = input(Fore.CYAN + "Entrez votre nom d'utilisateur: ")
+    password = getpass.getpass(Fore.CYAN + "Entrez votre mot de passe: ")
     
-    username = input("Entrez votre nom d'utilisateur: ")
-    password = getpass.getpass("Entrez votre mot de passe: ")
-    
-    cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
-    user = cursor.fetchone()
-    
-    if user:
-        print("Connexion réussie!")
+    if username in users and users[username] == password:
+        print(Fore.GREEN + "Connexion réussie!")
     else:
-        print("Erreur: Nom d'utilisateur ou mot de passe incorrect.")
-    
-    conn.close()
+        print(Fore.RED + "Erreur: Nom d'utilisateur ou mot de passe incorrect.")
 
 # Menu principal
 def main():
-    init_db()
-    
     while True:
-        print("\n1. Inscription")
-        print("2. Connexion")
-        print("3. Quitter")
-        choice = input("Choisissez une option: ")
+        print(Fore.YELLOW + "\n=== Menu Principal ===")
+        print(Fore.MAGENTA + "1. Inscription")
+        print(Fore.MAGENTA + "2. Connexion")
+        print(Fore.MAGENTA + "3. Quitter")
+        choice = input(Fore.CYAN + "Choisissez une option: ")
         
         if choice == '1':
             register_user()
         elif choice == '2':
             login_user()
         elif choice == '3':
-            print("Au revoir!")
+            print(Fore.YELLOW + "Au revoir!")
             break
         else:
-            print("Option invalide. Veuillez réessayer.")
+            print(Fore.RED + "Option invalide. Veuillez réessayer.")
 
 if __name__ == "__main__":
     main()
